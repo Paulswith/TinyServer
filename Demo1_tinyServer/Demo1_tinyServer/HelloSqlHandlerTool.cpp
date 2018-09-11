98faa5ed29bac73263fbb3e2270cc396c760e5d1
Paulswith <jevo747@gmail.com>
2018-09-12 01:14:21 +0800

    删除测试目录

--- Demo1_tinyServer/Demo1_tinyServer/HelloSqlHandlerTool.cpp
+++
@@ -1,8 +0,0 @@
-#include "HelloSqlHandlerTool.h"
-
-HelloSqlHandlerTool::HelloSqlHandlerTool(QString &sqlitePath, QString &sqlType)
-{
-    addDatabase(sqlType);
-    setDatabaseName(sqlitePath);
-    open();
-}
